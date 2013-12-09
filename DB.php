b44df76117f6d3ffe0d099c5d593a232bef187a9
byronmansfield <byron@byronmansfield.com>
2013-12-08 22:27:32 -0800

    Added databas php file

---
+++ DB.php
@@ -0,0 +1,13 @@
+<?php 
+
+  //--------------------------------------------------------------------------
+  // php script for fetching data from mysql database
+  //--------------------------------------------------------------------------
+  $host = "localhost";
+  $user = "bmans_byron";
+  $pass = "!xbm050183x!";
+
+  $databaseName = "bmans_byronmansfield";
+  $tableName = "users";
+
+?>

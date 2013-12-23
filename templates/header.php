ab43c54c7914d803ce27e6067853237190805c89
byronmansfield <byron@byronmansfield.com>
2013-12-22 18:29:38 -0800

    Added header.php file under templates directory

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<header id="header">
+  <section>
+	  <div class="title pull-left">
+	    <figure><a href="/">Title</a></figure>
+	    <figcaption>Tagline</figcaption>
+	  </div>
+
+	  <!-- [MINI NAV] -->
+    <nav class="pull-right">
+    	<div class="social pull-right">
+			  <a href="#" target="_blank"><img src="images/linkedin.png" alt="My Linked In Profile" /></a>
+			  <a href="#" target="_blank" class="behance"><img src="images/behance.png" alt="My Behance In Profile" /></a>
+	    </div>
+      <div class="contacts clearfix">
+		    <a href="tel:5555555555" class="phone">555-555-5555</a>
+			  <a rel="prettyPhoto" href="#?custom=true&width=600&height=530;" onclick="" class="email">name@example.com</a>
+			</div>
+    </nav>
+
+    <!-- [MAIN HORIZONTAL NAVIGATION] -->
+    <?PHP include 'templates/nav.html'; ?>
+
+  </section>
+</header>

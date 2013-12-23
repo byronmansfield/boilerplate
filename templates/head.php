ab9aab632975664d852f0d955185635188919dfe
byronmansfield <byron@byronmansfield.com>
2013-12-22 18:21:52 -0800

    Added template directory and head.php file

---
+++ templates/head.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="title" content="My Title">
+<meta name="description" content="My Description">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<link rel="shortcut icon" href="http://www.example.com/faviocon.ico">
+
+<title>My Title</title>
+
+<!-- [Styles] -->
+<link rel="stylesheet" type="text/css" media="screen" href="css/styles.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/placeholder.css">
+
+<!-- [Scripts] -->
+
+<!--[if lt IE 9]>
+<script src="js/html5shim.js"></script>
+<script src="js/IE9.js"></script>
+<![endif]-->
+
+<!-- [DATABASE CONNECTION] -->
+<?php include 'api.php'; ?>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/modernizr-1.0.min.js"></script>
